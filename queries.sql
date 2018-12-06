e593f132b5013768acd42c82bf872e9b1a22f765
Maria Vela <maria.vela790@gmail.com>
2018-12-06 15:42:16 -0600

    cleaned up insert statements

--- queries.sql
+++ queries.sql
@@ -1,32 +1,23 @@
 -- for a given ad, what is the email address of the user that created it
+# truncate table users, ads, category, ads_category;
 insert into users (username, email, password)
-values ('username', 'email', 'password1');
-
-insert into users (username, email, password)
-values ('username2', 'email2', 'password2');
+values ('username', 'email', 'password1'),
+('username2', 'email2', 'password2');
 
 insert into ads (title, description, user_id)
-values ('first post', 'description', 1);
-
-insert into ads (title, description, user_id)
-values ('second post', 'description', 2);
-
-insert into ads (title, description, user_id)
-values ('third post', 'description', 2);
+values ('first post', 'description', 1),
+       ('second post', 'description', 2),
+       ('third post', 'description', 2);
 
 insert into category (category)
-values ('category1');
-
-insert into category (category)
-values ('category2'),
+values ('category1'),
+       ('category2'),
        ('category3'),
        ('category4');
 
 insert into ads_category (ad_id, category_id)
-values (1, 1);
-
-insert into ads_category (ad_id, category_id)
-values (1, 2),
+values (1, 1),
+       (1, 2),
        (1, 4);
 
 select email from users
