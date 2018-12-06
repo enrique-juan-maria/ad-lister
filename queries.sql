22d2b76a239b2f16617e80b49af1a6f03e51ed2c
J.R <juan.delarosa1224@gmail.com>
2018-12-06 11:49:01 -0600

    changes cleared

--- queries.sql
+++ queries.sql
@@ -1,19 +1,39 @@
 -- for a given ad, what is the email address of the user that created it
+insert into users (username, email, password)
+values ('username', 'email', 'password1');
+
+insert into ads (title, description, user_id)
+values ('first post', 'description', 1);
+
+insert into category (category)
+values ('category1');
+
+insert into category (category)
+values ('category2'),
+       ('category3'),
+       ('category4');
+
+insert into ads_category (ad_id, category_id)
+values (1, 1);
+
+insert into ads_category (ad_id, category_id)
+values (1, 2),
+       (1, 4);
 
 select email from users
 where id in (
-    select user_id from ads
-    where id in(
-        select id from ads
-        where title = 'given title'
-    )
-);
+  select user_id from ads
+  where id in(
+      select id from ads
+      where title = 'given title'
+      )
+  );
 
 select category from category
 where id in (
-    select category_id from ads_category
-    where ad_id in(
-        select id from ads
-        where title = 'given title'
-    )
-);
+  select category_id from ads_category
+  where ad_id in(
+      select id from ads
+      where title = 'first post'
+      )
+  );
